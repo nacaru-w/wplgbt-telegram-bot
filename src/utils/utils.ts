33df1b6b32b2cde1e36a9bbe5a283aa92169f2f1
nacaru-w <ignaciocr92@gmail.com>
2024-08-03 02:42:25 +0200

    Adding new utility functions

---
+++ src/utils/utils.ts
@@ -0,0 +1,41 @@
+import { Mes } from "../types/bot-types";
+import { countryISOCodes } from "./iso-countries";
+
+function getCurrentYear(): string {
+    const currentYear = new Date().getFullYear();
+    return currentYear.toString();
+}
+
+function getCurrentMonthInSpanish(): Mes {
+    const monthsInSpanish: Mes[] = [
+        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ];
+    const currentMonthIndex = new Date().getMonth();
+    return monthsInSpanish[currentMonthIndex];
+}
+
+export function getCountryOnISO(ISO: string | null): string | null {
+    if (ISO == null) {
+        return null
+    }
+    return countryISOCodes[ISO] || null;
+}
+
+export function removeDoubleSquareBrackets(input: string | null): string | null {
+    if (!input) {
+        return null
+    }
+
+    const regex = /^\{\{(.*?)\}\}$/;
+    const match = input.match(regex);
+
+    if (match) {
+        return match[1];
+    }
+
+    return input;
+}
+
+export const currentYear: string = getCurrentYear();
+export const currentMonth: Mes = getCurrentMonthInSpanish();
